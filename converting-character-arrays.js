e68808b529dcfcd1e9e24c00016db302a859dd2c
Brooke F <brooke.floyd10@gmail.com>
2026-04-30 04:05:25 -0500

    reversed messages  and created 2 reversed messages

---
+++ converting-character-arrays.js
@@ -0,0 +1,26 @@
+ 
+let reversedMessage = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC"
+let message1 = reversedMessage.split('').reverse().join('');
+console.log(message1);
+
+
+let reversedMessage2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB"
+let message2 = reversedMessage2.split('').reverse().join('');
+console.log(message2);
+
+let reversedMessage3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI.lanoisseforp a ekil leef ot evah t'nod uoY"
+let message3 = reversedMessage3.split('').reverse().join('');
+console.log(message3);
+
+let reversedMessage4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ"
+let message4 = reversedMessage4.split('').reverse().join('')
+console.log(message4);
+
+
+let myMessage1 = "Coding is hard but we are all in this together!"
+let myReversedMessage1 = myMessage1.split('').reverse().join('');
+console.log(myReversedMessage1);
+
+let myMessage2 = "If you feel like you are failing try to remember, you aren't failing you're just collecting data...literally.";
+let myReversedMessage2 = myMessage2.split('').reverse().join('');
+console.log(myReversedMessage2);
